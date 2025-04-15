3448f85e73803631c89d4f0f006d19a866a33a4d
vidimamisokuhle <vidimamisokuhle@gmail.com>
2025-04-15 19:46:53 +0200

    added search event function

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "ab3defb06170c8ob2cccb493b58t53ba";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
